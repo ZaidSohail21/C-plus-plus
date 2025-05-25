bde31a4a75c53236644d96b2a9c39eafbc502897
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:13:29 +0500

    Adding 1 in  Search value in Array from User

---
+++ Question no 3.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int Array1[10];
+//	int sum = 0;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cin >> Array1[i];
+//	}
+//	cout << "Array1 [10]={";
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cout << Array1[i]<< ",";
+//	}
+//	cout << "}";
+//	cout << "\n";
+//	int x;
+//	cout << "Enter a number :";
+//	cin >> x;
+//	for (int j = 0; j <= 9; j++)
+//	{
+//
+//		if (Array1[j] == x)
+//		{
+//			sum = sum + 1;
+//		}
+//	}
+//	cout << "Number is present " << sum << " times";
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
