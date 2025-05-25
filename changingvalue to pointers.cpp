88af491b11849bf27a18b3b1effe3b8fd827b5a0
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:39:51 +0500

    Changing Simple Int variable to pointer

---
+++ changingvalue to pointers.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int x = 14;
+//	int *ptr = &x;
+//
+//	cout << "The value of x is " << x<< endl;
+//	cout << "The value of ptr is " << *ptr<< endl;
+//	// changing byy value
+//	x = 1;
+//	cout << "The value of x is " << x << endl;
+//	cout << "The value of ptr is " << *ptr << endl;
+//	//changing by pointer
+//	*ptr = 3;
+//	cout << "The value of x is " << x << endl;
+//	cout << "The value of ptr is " << *ptr << endl;
+//	system("pause");
+//	return 0;
+//}
