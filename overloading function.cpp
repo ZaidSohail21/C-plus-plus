d0ddba01123f76a0f23063ddace8b8e2cdb839a6
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:44:11 +0500

    Overloading function using same name/diff parametr

---
+++ overloading function.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//
+//int sum(int a, int b);
+//double sum(double a, double b);
+//float sum(float a, float b);
+//
+//int main()
+//{
+//	int x ;
+//	cout << "Enter the value of x :";
+//	cin >> x;
+//	int y ;
+//	cout << "Enter the value of y :";
+//	cin >> y;
+//	int s = sum(x, y);
+//
+//	
+//
+//	
+//
+//
+//
+//	cout << "hello world";
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//
+//}
