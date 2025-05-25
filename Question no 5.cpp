0a7a086440090025af955e4cf23169573f6e01c9
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:15:05 +0500

    Detecting the even value and index in array

---
+++ Question no 5.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int Array1[10];
+//	int sum=0;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cin >> Array1[i];
+//	}
+//	cout << "Array1 [10]={";
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cout << Array1[i] << ",";
+//	}
+//	cout << "}";
+//	cout << "\n";
+//	for (int i = 0; i <= 9; i++)
+//	{
+//		if (Array1[i] % 2 == 0&& Array1[i%2==0])
+//		{
+//			sum = sum + 1;
+//		}
+//	}
+//	cout << "The even numbers in even index is  :" << sum << "\n";
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
