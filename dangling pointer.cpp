c726584d1a1d4367aa77cda113e43ecc778af71c
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:41:20 +0500

    Basic Concepts of how dangling pointer works

---
+++ dangling pointer.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int A[100];
+//	int*B=new int(5);
+//	for (int i = 0; i < 100; i++)
+//	{
+//		A[i] =  i;
+//	}
+//	B = A;
+//	for (int i = 0; i < 100; i++)
+//	{
+//		cout << A[i] << "   ";
+//	}
+//	for (int i = 0; i < 100; i++)
+//	{
+//		cout << B[i] << "  "; 
+//	}
+//	delete B;
+//	for (int i = 0; i < 100; i++)
+//	{
+//		cout << A[i] << "   ";
+//	}
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//
+//}
