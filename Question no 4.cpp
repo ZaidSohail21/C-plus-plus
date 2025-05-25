294182dd8d45a390fae36918ad536b65383e7c09
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:14:10 +0500

    maximum and Mininmum Value in Array

---
+++ Question no 4.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int Array1[10];
+//	int max;
+//	int min;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cin >> Array1[i];
+//	}
+//	cout << "Array1 [10]={";
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cout << Array1[i] << ",";
+//	}
+//	cout << "}";
+//	cout << "\n";
+//	for (int i = 0; i <= 9; i++)
+//	{
+//		max = Array1[0];
+//		if (Array1[0]<Array1[i])
+//		{
+//			max=Array1[i];
+//		}
+//		min = Array1[0];
+//		if (Array1[0] > Array1[i])
+//		{
+//			min = Array1[i];
+//		}
+//	}
+//	cout << "Maximum Number is :" << max<< "\n";
+//	cout << "Minimum Number is :" << min<<"\n";
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
