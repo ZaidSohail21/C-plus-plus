7b5740651a5d41db1a1a2d7f4fb17e3393a99353
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 17:56:24 +0500

    Simple calculator (Taking values from user)

---
+++ TASK 7 .cpp
@@ -0,0 +1,32 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << " Enter value of x:";
+	cin >> x;
+	int y;
+	cout << " Enter value of y:";
+	cin >> y;
+	int sum = 0;
+	sum = x + y;
+	cout << " sum of value:";
+	cout << sum;
+	int Difference;
+	Difference = x - y;
+	cout << "\n Difference of value :";
+	cout << Difference;
+	int Product;
+	Product = x * y;
+	cout << "\n Product of value:";
+	cout << Product ;
+	int Division;
+  	Division = x / y;
+	cout << "\n Division of value:";
+	cout << Division;
+	cout << "\n";
+	system(" pause");
+	return 0;
+}*/ 
