dfd388c06487da92a655792bc747bec85936501b
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:37:12 +0500

    Contact Database using filehandling

---
+++ file writing 2.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//#include<fstream>
+//#include<cstring>
+//using namespace std;
+//int main()
+//{
+//	string names[1000] ;
+//	string num[1000];
+//	ofstream fout;
+//	fout.open("contacts.txt");
+//	int i = 0;
+//	char a = 'y';
+//	while (a == 'y')
+//	{
+//		cout << "Enter the name of the person: ";
+//		cin >> names[i];
+//		fout << names[i] << " ";
+//		cout << "Enter the number of the contacts: ";
+//		cin >> num[i];
+//		fout << num[i];
+//		fout << endl;
+//		cout << "Enter the choice that you want to continue or not(y or n)";
+//		cin >> a;
+//		i++;
+//		cout << endl;
+//		//	    fout<<endl;
+//	}
+//
+//	fout.close();
+//	return 0;
+//}
