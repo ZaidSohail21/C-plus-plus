d1a1ac2600ed03b80df61afc8fae3e5ceab5bab3
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:12:14 +0500

    Linear Search

---
+++ Question no 2.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int Array1[10];
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cin >> Array1[i];
+//	}
+//	cout << "Array1 [10]={";
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cout << Array1[i]<< ",";
+//	}
+//	cout << "}";
+//	cout << "\n";
+//	int x;
+//	cout << "Enter a number :";
+//	cin >> x;
+//	for (int j = 0; j <= 9; j++)
+//	{
+//		if (Array1[j] == x)
+//		{
+//			cout << "Number is present in index :" << j<< endl;
+//		}
+//	}
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
