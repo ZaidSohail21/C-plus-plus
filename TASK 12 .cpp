d261f0e5a8aaf0279c86fbf9334b8edb73f2e1ce
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 17:57:55 +0500

    Area Calculator

---
+++ TASK 12 .cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int Length;
+//	cout << " Length of the Rectangle :";
+//	cin >> Length;
+//	int  Width;
+//	cout << " Width of the Rectangle :";
+//	cin >> Width;
+//	int Perimeter;
+//	Perimeter = 2 * (Length + Width);
+//	cout << " Perimeter of the Rectangle :";
+//	cout << Perimeter;
+//	cout << "\n";
+//	int Area;
+//	Area = Length*Width;
+//	cout << " Area of the Ractangle :";
+//	cout << Area;
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
+//
