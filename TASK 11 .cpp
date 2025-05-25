d0cf113896237ad5c03782a850246cf820dff301
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 17:57:19 +0500

    Minute calculator

---
+++ TASK 11 .cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int Minutes;
+//	int Hours;
+//	int r;
+//	cout << " ENTER Minutes :";
+//	cin >> Minutes;
+//	Hours = Minutes / 60;
+//	cout << Hours;
+//	cout << " Hours,";
+//	r = Minutes - Hours*60;
+//	cout <<r;
+//	cout << " Minutes";
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
