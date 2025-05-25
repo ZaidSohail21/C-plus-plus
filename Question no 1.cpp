1dece8a9f9d7be83ac207c484d5ab65621f1b00c
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:11:35 +0500

    Reversing the Array

---
+++ Question no 1.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	
+//	int Array1[10] = { 2,4,5,66,75,8,53,11,3,45 };
+//	int Array2[10] = {};
+//	cout << "Array1[10] = { 2,4,5,66,75,8,53,11,3,45 }"<<endl;
+//	cout << "Array2[10]={";
+//	for (int i = 9; i >=0; i--)
+//	{
+//		cout << Array1[i];
+//		cout << ",";
+//	}
+//	cout << "}";
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
