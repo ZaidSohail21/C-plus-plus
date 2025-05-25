3514284fe3685f029e5761ef1c526950bacfc078
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 17:59:21 +0500

    Billing System

---
+++ TASK 9 .cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int Fuel;
+//	cout << " ENTER VALUE OF Fuel :";
+//	cin >> Fuel;
+//	int Rent;
+//	cout << " ENTER VALUE OF Rent :";
+//	cin >> Rent;
+//	int Bills ;
+//	cout << " ENTER VALUE OF Bills :";
+//	cin >> Bills ;
+//	cout << "___________________________________________________\n";
+//	cout << " FUEL:";
+//	cout << Fuel;
+//	cout << "\n";
+//	cout << "\n Rent: ";
+//	cout << Rent;
+//	cout << "\n";
+//	cout << " \n Bills:";
+//	cout << Bills;
+//	cout << "\n";
+//	int Total;
+//	Total = Fuel + Rent + Bills;
+//	cout << " \n Total :";
+//	cout << Total;
+//	cout << "\n__________________________________________________\n";
+//    system("\n pause");
+//	return 0;
+//}
