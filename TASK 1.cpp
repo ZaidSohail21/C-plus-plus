0a219b38343fd9e65c47d8383d581f747a69010f
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 17:55:12 +0500

    Simple iostream tasks(Hello World)

---
+++ TASK 1.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "  ************    \n";
+//	cout << "  HELLO WORLD     \n";
+//	cout << " *************  \n";
+//	system("pause");
+//	return 0;
+//}
