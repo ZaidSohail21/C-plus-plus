4a77a8e3cb9b9b947a843133799adeb25a7d29bf
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 18:16:05 +0500

    Detecting the secong largest value in C++

---
+++ Question no 7.cpp
@@ -0,0 +1,41 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int Array1[10];
+//	int max;
+//	int slarge;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cin >> Array1[i];
+//	}
+//	cout << "Array1 [10]={";
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cout << Array1[i] << ",";
+//	}
+//	cout << "}";
+//	cout << "\n";
+//	for (int i = 0; i <= 9; i++)
+//	{
+//		max = Array1[0];
+//		if (Array1[0]<Array1[i])
+//		{
+//			max=Array1[i];
+//		}
+//		slarge = Array1[0];
+//		for (i = 0; i < 10; i++)
+//		{
+//			if (slarge < Array1[i])
+//			{
+//				if (Array1[i] != max)
+//					slarge = Array1[i];
+//			}
+//		
+//	}
+//	cout << "Maximum Number is :" << slarge<< "\n";
+//	
+//	cout << "\n";
+//	system("pause");
+//	return 0;
+//}
